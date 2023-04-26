be312ec3c9c1ad1975e78ccef3e130e28d2bba3f
axelwiumarneson <129280003+axelwiumarneson@users.noreply.github.com>
2023-04-26 08:53:54 +0200

    Legger til

---
+++ JavaScript/game2.js
@@ -0,0 +1,903 @@
+let elements = [
+
+    {
+        name: "hydrogen",
+        symbol: "H",
+        number: 1
+    },
+
+    {
+        name: "helium",
+        symbol: "He",
+        number: 2
+    },
+
+    {
+        name: "litium",
+        symbol: "Li",
+        number: 3
+    },
+
+    {
+        name: "beryllium",
+        symbol: "Be",
+        number: 4
+    },
+
+    {
+        name: "bor",
+        symbol: "B",
+        number: 5
+    },
+
+    {
+        name: "karbon",
+        symbol: "C",
+        number: 6
+    },
+
+    {
+        name: "nitrogen",
+        symbol: "N",
+        number: 7
+    },
+
+    {
+        name: "oksygen",
+        symbol: "O",
+        number: 8
+    },
+
+    {
+        name: "fluor",
+        symbol: "F",
+        number: 9
+    },
+
+    {
+        name: "neon",
+        symbol: "Ne",
+        number: 10
+    },
+
+    {
+        name: "natrium",
+        symbol: "Na",
+        number: 11
+    },
+
+    {
+        name: "magnesium",
+        symbol: "Mg",
+        number: 12
+    },
+
+    {
+        name: "aluminium",
+        symbol: "Al",
+        number: 13
+    },
+
+    {
+        name: "silisium",
+        symbol: "Si",
+        number: 14
+    },
+
+    {
+        name: "fosfor",
+        symbol: "P",
+        number: 15
+    },
+
+    {
+        name: "svovel",
+        symbol: "S",
+        number: 16
+    },
+
+    {
+        name: "klor",
+        symbol: "Cl",
+        number: 17
+    },
+
+    {
+        name: "argon",
+        symbol: "Ar",
+        number: 18
+    },
+
+    {
+        name: "kalium",
+        symbol: "K",
+        number: 19
+    },
+
+    {
+        name: "kalsium",
+        symbol: "Ca",
+        number: 20
+    },
+
+    {
+        name: "scandium",
+        symbol: "Sc",
+        number: 21
+    },
+
+    {
+        name: "titan",
+        symbol: "Ti",
+        number: 22
+    },
+
+    {
+        name: "vanadium",
+        symbol: "V",
+        number: 23
+    },
+
+    {
+        name: "krom",
+        symbol: "Cr",
+        number: 24
+    },
+
+    {
+        name: "mangan",
+        symbol: "Mn",
+        number: 25
+    },
+
+    {
+        name: "jern",
+        symbol: "Fe",
+        number: 26
+    },
+
+    {
+        name: "kobolt",
+        symbol: "Co",
+        number: 27
+    },
+
+    {
+        name: "nikkel",
+        symbol: "Ni",
+        number: 28
+    },
+
+    {
+        name: "kobber",
+        symbol: "Cu",
+        number: 29
+    },
+
+    {
+        name: "sink",
+        symbol: "Zn",
+        number: 30
+    },
+
+    {
+        name: "gallium",
+        symbol: "Ga",
+        number: 31
+    },
+
+    {
+        name: "germanium",
+        symbol: "Ge",
+        number: 32
+    },
+
+    {
+        name: "arsen",
+        symbol: "As",
+        number: 33
+    },
+
+    {
+        name: "selen",
+        symbol: "Se",
+        number: 34
+    },
+
+    {
+        name: "brom",
+        symbol: "Br",
+        number: 35
+    },
+
+    {
+        name: "krypton",
+        symbol: "Kr",
+        number: 36
+    },
+
+    {
+        name: "rubidium",
+        symbol: "Rb",
+        number: 37
+    },
+
+    {
+        name: "strontium",
+        symbol: "Sr",
+        number: 38
+    },
+
+    {
+        name: "yttrium",
+        symbol: "Y",
+        number: 39
+    },
+
+    {
+        name: "zirkonium",
+        symbol: "Zr",
+        number: 40
+    },
+
+    {
+        name: "niob",
+        symbol: "Nb",
+        number: 41
+    },
+
+    {
+        name: "molybden",
+        symbol: "Mo",
+        number: 42
+    },
+
+    {
+        name: "technetium",
+        symbol: "Tc",
+        number: 43
+    },
+
+    {
+        name: "ruthenium",
+        symbol: "Ru",
+        number: 44
+    },
+
+    {
+        name: "rhodium",
+        symbol: "Rh",
+        number: 45
+    },
+
+    {
+        name: "palladium",
+        symbol: "Pd",
+        number: 46
+    },
+
+    {
+        name: "sølv",
+        symbol: "Ag",
+        number: 47
+    },
+
+    {
+        name: "kadmium",
+        symbol: "Cd",
+        number: 48
+    },
+
+    {
+        name: "indium",
+        symbol: "In",
+        number: 49
+    },
+
+    {
+        name: "tinn",
+        symbol: "Sn",
+        number: 50
+    },
+
+    {
+        name: "antimon",
+        symbol: "Sb",
+        number: 51
+    },
+
+    {
+        name: "tellur",
+        symbol: "Te",
+        number: 52
+    },
+
+    {
+        name: "jod",
+        symbol: "I",
+        number: 53
+    },
+
+    {
+        name: "xenon",
+        symbol: "Xe",
+        number: 54
+    },
+
+    {
+        name: "cesium",
+        symbol: "Cs",
+        number: 55
+    },
+
+    {
+        name: "barium",
+        symbol: "Ba",
+        number: 56
+    },
+
+    {
+        name: "lantan",
+        symbol: "La",
+        number: 57
+    },
+
+    {
+        name: "cerium",
+        symbol: "Ce",
+        number: 58
+    },
+
+    {
+        name: "praseodym",
+        symbol: "Pr",
+        number: 59
+    },
+
+    {
+        name: "neodym",
+        symbol: "Nd",
+        number: 60
+    },
+
+    {
+        name: "promethium",
+        symbol: "Pm",
+        number: 61
+    },
+
+    {
+        name: "samarium",
+        symbol: "Sm",
+        number: 62
+    },
+
+    {
+        name: "europium",
+        symbol: "Eu",
+        number: 63
+    },
+
+    {
+        name: "gadolinium",
+        symbol: "Gd",
+        number: 64
+    },
+
+    {
+        name: "terbium",
+        symbol: "Tb",
+        number: 65
+    },
+
+    {
+        name: "dysprosium",
+        symbol: "Dy",
+        number: 66
+    },
+
+    {
+        name: "holmium",
+        symbol: "Ho",
+        number: 67
+    },
+
+    {
+        name: "erbium",
+        symbol: "Er",
+        number: 68
+    },
+
+    {
+        name: "thulium",
+        symbol: "Tm",
+        number: 69
+    },
+
+    {
+        name: "ytterbium",
+        symbol: "Yb",
+        number: 70
+    },
+
+    {
+        name: "lutetium",
+        symbol: "Lu",
+        number: 71
+    },
+
+    {
+        name: "hafnium",
+        symbol: "Hf",
+        number: 72
+    },
+
+    {
+        name: "tantal",
+        symbol: "Ta",
+        number: 73
+    },
+
+    {
+        name: "wolfram",
+        symbol: "W",
+        number: 74
+    },
+
+    {
+        name: "rhenium",
+        symbol: "Re",
+        number: 75
+    },
+
+    {
+        name: "osmium",
+        symbol: "Os",
+        number: 76
+    },
+
+    {
+        name: "iridium",
+        symbol: "Ir",
+        number: 77
+    },
+
+    {
+        name: "platina",
+        symbol: "Pt",
+        number: 78
+    },
+
+    {
+        name: "gull",
+        symbol: "Au",
+        number: 79
+    },
+
+    {
+        name: "kvikksølv",
+        symbol: "Hg",
+        number: 80
+    },
+
+    {
+        name: "thallium",
+        symbol: "Tl",
+        number: 81
+    },
+
+    {
+        name: "bly",
+        symbol: "Pb",
+        number: 82
+    },
+
+    {
+        name: "vismut",
+        symbol: "Bi",
+        number: 83
+    },
+
+    {
+        name: "polonium",
+        symbol: "Po",
+        number: 84
+    },
+
+    {
+        name: "astat",
+        symbol: "At",
+        number: 85
+    },
+
+    {
+        name: "radon",
+        symbol: "Rn",
+        number: 86
+    },
+
+    {
+        name: "francium",
+        symbol: "Fr",
+        number: 87
+    },
+
+    {
+        name: "radium",
+        symbol: "Ra",
+        number: 88
+    },
+
+    {
+        name: "actinium",
+        symbol: "Ac",
+        number: 89
+    },
+
+    {
+        name: "thorium",
+        symbol: "Th",
+        number: 90
+    },
+
+    {
+        name: "protactinium",
+        symbol: "Pa",
+        number: 91
+    },
+
+    {
+        name: "uran",
+        symbol: "U",
+        number: 92
+    },
+
+    {
+        name: "neptunium",
+        symbol: "Np",
+        number: 93
+    },
+
+    {
+        name: "plutonium",
+        symbol: "Pu",
+        number: 94
+    },
+
+    {
+        name: "americium",
+        symbol: "Am",
+        number: 95
+    },
+
+    {
+        name: "curium",
+        symbol: "Cm",
+        number: 96
+    },
+
+    {
+        name: "berkelium",
+        symbol: "Bk",
+        number: 97
+    },
+
+    {
+        name: "californium",
+        symbol: "Cf",
+        number: 98
+    },
+
+    {
+        name: "einsteinium",
+        symbol: "Es",
+        number: 99
+    },
+
+    {
+        name: "fermium",
+        symbol: "Fm",
+        number: 100
+    },
+
+    {
+        name: "mendelevium",
+        symbol: "Md",
+        number: 101
+    },
+
+    {
+        name: "nobelium",
+        symbol: "No",
+        number: 102
+    },
+
+    {
+        name: "lawrencium",
+        symbol: "Lr",
+        number: 103
+    },
+
+    {
+        name: "rutherfordium",
+        symbol: "Rf",
+        number: 104
+    },
+
+    {
+        name: "dubnium",
+        symbol: "Db",
+        number: 105
+    },
+
+    {
+        name: "seaborgium",
+        symbol: "Sg",
+        number: 106
+    },
+
+    {
+        name: "bohrium",
+        symbol: "Bh",
+        number: 107
+    },
+
+    {
+        name: "hassium",
+        symbol: "Hs",
+        number: 108
+    },
+
+    {
+        name: "meitnerium",
+        symbol: "Mt",
+        number: 109
+    },
+
+    {
+        name: "darmstadtium",
+        symbol: "Ds",
+        number: 110
+    },
+
+    {
+        name: "røntgenium",
+        symbol: "Rg",
+        number: 111
+    },
+
+    {
+        name: "copernicium",
+        symbol: "Cn",
+        number: 112
+    },
+
+    {
+        name: "nihonium",
+        symbol: "Nh",
+        number: 113
+    },
+
+    {
+        name: "flerovium",
+        symbol: "Fl",
+        number: 114
+    },
+
+    {
+        name: "moscovium",
+        symbol: "Mc",
+        number: 115
+    },
+
+    {
+        name: "livermorium",
+        symbol: "Lv",
+        number: 116
+    },
+
+    {
+        name: "tenness",
+        symbol: "Ts",
+        number: 117
+    },
+
+    {
+        name: "oganesson",
+        symbol: "Og",
+        number: 118
+    },
+]
+
+//legger inn verdiene til grunnstoffene i periodesystemet
+for (let grunnstoff of elements) {
+    let el = document.getElementsByClassName(grunnstoff.name)[0];
+    el.innerHTML = `<p class="number">${grunnstoff.number}</p>
+    <p class="symbol">${grunnstoff.symbol}</p>
+    <p class="name">${grunnstoff.name}</p>`
+}
+
+//random number generator
+
+var numbers = [];
+for (var i = 0; i <= 117; i++) {
+  numbers.push(i);
+}
+
+
+var randomNumber 
+
+function getRandomNumber() {
+  if (numbers.length === 0) {
+    // All numbers have been generated
+    return null;
+  }
+
+  var randomIndex = Math.floor(Math.random() * numbers.length);
+  randomNumber = numbers[randomIndex];
+  // Remove the generated number from the array so that it cannot be generated again
+  numbers.splice(randomIndex, 1);
+
+  return randomNumber;
+}
+
+
+
+// Henter elementer fra topp
+let trykkPaaEl = document.querySelector('.trykkPaa')
+let skipBtn = document.querySelector('.skip')
+let tellerEl = document.querySelector('.teller')
+
+let antall_riktige = 0
+
+tellerEl.innerHTML = `${antall_riktige}/118`
+
+/* function trykkPaa(){
+    trykkPaaEl.innerHTML = `Trykk på ${elements[getRandomNumber()].name}!`
+} */
+
+//Legger til lytter på skip knappen
+/* skipBtn.addEventListener('click', neste)
+
+function neste(){
+    if(timeEl.textContent != '00:00:00'){
+        trykkPaa()
+    }
+} */
+
+//Timer
+
+const timeEl = document.querySelector('.frame .top .timer')
+
+let seconds = 0
+let intervall = null
+
+function timer (){
+    seconds++
+    let hrs = Math.floor(seconds / 3600)
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60)
+    let secs = seconds % 60
+    
+    if (secs < 10) secs = '0' + secs
+    if (mins < 10) mins = '0' + mins
+    if (hrs < 10) hrs = '0' + hrs
+
+    timeEl.innerHTML = `${hrs}:${mins}:${secs}`
+}
+
+function startTimer () {
+    if (intervall) {
+        return
+    }
+
+    intervall = setInterval(timer, 1000)
+}
+
+function stopTimer () {
+    clearInterval(intervall)
+    intervall = null
+
+}
+
+function resetTimer () {
+    stop()
+    seconds = 0
+    timeEl.innerHTML = '00:00:00'
+}
+
+// Starter spillet
+
+let startBtn = document.querySelector('.start')
+
+startBtn.addEventListener('click', startSpill)
+
+function startSpill(){
+
+    trykkPaaEl.innerHTML = `Trykk på ${elements[getRandomNumber()].symbol}!`
+
+    // Starter timeren
+    startTimer()
+
+    // Gjemmer navnet på grunnstoffene
+    for (let grunnstoff of elements) {
+        let el = document.getElementsByClassName(grunnstoff.name)[0];
+        el.innerHTML = `<p class="number">${grunnstoff.number}</p>
+        <p class="symbol">?</p>
+        <p class="name">${grunnstoff.name} </p>`
+    }
+}
+
+// selve spillet
+
+//Henter elementene i periodesystemet
+let grunnstoffEls = document.querySelectorAll('.grunnstoff')/* [1].innerHTML = 'hei' */
+
+function sjekkSvar(e) {
+    
+    //finner det riktige elementet
+    let correctElement = String(`grunnstoff ${elements[randomNumber].name}`)
+    //finner elementet som ble trykket på
+    let clickedElement = String(e.target.getAttribute('class'))
+    if(e.target.classList.contains(`margin_top grunnstoff ${elements[randomNumber].name}`) || e.target.parentElement.classList.contains(elements[randomNumber].name)){
+        clickedElement = `grunnstoff ${elements[randomNumber].name}`
+    }else if(e.target.classList.contains('done')||e.target.parentElement.classList.contains('done')){
+        
+    }
+
+
+    console.log(correctElement)
+    console.log(clickedElement)
+
+    if(clickedElement === correctElement){
+        if(e.target.classList.contains('symbol') || e.target.classList.contains('name') || e.target.classList.contains('number')){
+            e.target.parentElement.classList.add('done')
+            e.target.parentElement.style.backgroundColor = 'green'
+            e.target.parentElement.innerHTML = `<p class="number">${elements[randomNumber].number}</p>
+            <p class="symbol">${elements[randomNumber].symbol}</p>
+            <p class="name">${elements[randomNumber].name}</p>`
+            tellerEl.innerHTML = `${antall_riktige + 1}/118`
+            antall_riktige++
+        }else{
+        e.target.classList.add('done')
+        e.target.style.backgroundColor = 'green'
+        e.target.innerHTML = `<p class="number">${elements[randomNumber].number}</p>
+        <p class="symbol">${elements[randomNumber].symbol}</p>
+        <p class="name">${elements[randomNumber].name}</p>`
+        tellerEl.innerHTML = `${antall_riktige + 1}/118`
+        antall_riktige++
+        }
+    }else{
+        if(e.target.classList.contains('symbol') || e.target.classList.contains('name') || e.target.classList.contains('number')){
+            e.target.parentElement.classList.add('done')
+            e.target.parentElement.style.backgroundColor = 'red'
+            e.target.parentElement.innerHTML = ""
+        }else{
+            e.target.classList.add('done')
+            e.target.style.backgroundColor = 'red'
+            e.target.innerHTML = ""
+        }
+    }
+ 
+    trykkPaaEl.innerHTML = `Trykk på ${elements[getRandomNumber()].symbol}!`
+
+    if(numbers.length === 0 && grunnstoffEls[randomNumber].classList.contains('done')){
+        sluttspillet()
+    }
+}
+
+grunnstoffEls.forEach(element => {
+    element.addEventListener('click', sjekkSvar)
+})
+
+
+
+let frameEl = document.querySelector('.frame')
+
+function sluttspillet(){
+    stopTimer()
+
+    frameEl.classList.toggle('opacity')
+}
+
+
+
